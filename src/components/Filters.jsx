14bdda2bc4c5bf38bc5852e4928d6791f79b487e
Nick Gannon <nmgannon13@gmail.com>
2024-09-09 11:39:00 -0400

    Added and changed to Inter font

--- src/components/Filters.jsx
+++ src/components/Filters.jsx
@@ -2,7 +2,7 @@ import Button from "./Button"
 
 export default function Filters() {
     return (
-        <section className="flex justify-center gap-4 my-10">
+        <section className="flex justify-center gap-4 my-10 font-Inter">
             <Button buttonText="All" />
             <Button buttonText="Fonts" />
             <Button buttonText="Foundries" />
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,6 +1,6 @@
 export default function Hero() {
     return (
-        <section className="text-center">
+        <section className="text-center font-Inter">
             <h1 className="font-bold text-4xl md:text-6xl">Font Resources</h1>
             <p className="font-light text-base md:text-xl mt-4">Curated catalog of high quality typography services and collections that have practical use</p>
         </section>
