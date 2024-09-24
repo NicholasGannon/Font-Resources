e216412e17012f5d78ada5d05eb888bac54248b5
Nick Gannon <nmgannon13@gmail.com>
2024-09-24 11:22:57 -0400

    Button responsiveness styling

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,5 +1,5 @@
 export default function Button({buttonText}) {
     return (
-        <button className="px-4 py-2 bg-gray-100 border border-gray-400 rounded-sm hover:bg-gray-200">{buttonText}</button>
+        <button className="text-sm sm:text-lg px-2 py-1 sm:px-4 sm:py-2 bg-gray-100 border border-gray-400 rounded-sm hover:bg-gray-200">{buttonText}</button>
     )
 }
