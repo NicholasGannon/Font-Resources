ee88b8f4adfbf7f690e80b1c77604737db1dc1f6
Nick Gannon <nmgannon13@gmail.com>
2024-10-02 09:40:19 -0400

    Card hover animation changed slightly

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,6 +1,6 @@
 export default function Card({imageLink, image, altDescription}) {
     return (
-        <a href={imageLink} className="hover:cursor-pointer hover:brightness-75" target="_blank">
+        <a href={imageLink} className="hover:cursor-pointer hover:brightness-50 duration-300" target="_blank">
             <img src={image} alt={altDescription} />
         </a>
     )
